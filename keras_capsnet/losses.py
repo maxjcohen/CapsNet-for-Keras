8730524c8fdbaaac5c2ee719c3144372022edee6
Max Cohen <max.cohen@gmx.com>
2018-09-29 11:31:11 +0200

    Added margin loss

---
+++ keras_capsnet/losses.py
@@ -0,0 +1,7 @@
+import keras.backend as K
+
+def margin(margin=0.9, lambdas=0.5):
+    def loss(y_true, y_pred):
+        L_c = y_true * K.square(K.maximum(0.0, margin - y_pred)) + lambdas * (1 - y_true) * K.square(K.maximum(0.0, y_pred - margin))
+        return K.mean(K.sum(L_c, axis=1))
+    return loss
