19df447ffcf066664a52ec80d70e80e25b03cee7
Max Cohen <max.cohen@gmx.com>
2018-09-30 12:06:12 +0200

    Callback now support dynamic number of metrics

--- utils.py
+++ utils.py
@@ -2,12 +2,16 @@ from matplotlib import pyplot as plt
 from keras.callbacks import Callback
 
 class LossHistory(Callback):
-    def on_train_begin(self, logs={}):
-        self.loss = []
-        self.acc = []
+    def __init__(self, metrics, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.metrics = {}
+        for metric in metrics:
+            self.metrics[metric] = []
+
     def on_batch_end(self, batch, logs={}):
-        self.loss.append(logs.get('loss'))
-        self.acc.append(logs.get('acc'))
+        for key, metric in self.metrics.items():
+            self.metrics[key].append( logs.get(key) )
 
 def plotHistory(loss, acc):
     plt.figure(figsize=(20, 4))
