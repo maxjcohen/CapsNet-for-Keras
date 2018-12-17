5ed56f3e86835b2d46cad771e9e01d3e651a76b2
EC2 Default User <ec2-user@ip-172-16-87-72.us-east-2.compute.internal>
2018-12-17 09:52:16 +0000

    fixed major mistake in margin loss

--- keras_capsnet/losses.py
+++ keras_capsnet/losses.py
@@ -2,6 +2,6 @@ import keras.backend as K
 
 def margin(margin=0.9, lambdas=0.5):
     def loss(y_true, y_pred):
-        L_c = y_true * K.square(K.maximum(0.0, margin - y_pred)) + lambdas * (1 - y_true) * K.square(K.maximum(0.0, y_pred - margin))
+        L_c = y_true * K.square(K.maximum(0.0, margin - y_pred)) + lambdas * (1 - y_true) * K.square(K.maximum(0.0, y_pred - (1.0 - margin)))
         return K.mean(K.sum(L_c, axis=1))
     return loss
