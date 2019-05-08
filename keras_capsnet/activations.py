4c0c8c66d68742c05217c8d69198c0a7ecabdd25
EC2 Default User <ec2-user@ip-172-16-52-179.us-east-2.compute.internal>
2019-05-08 09:38:23 +0000

    changed activation function paramters following https://keras.io/examples/cifar10_cnn_capsule/

--- keras_capsnet/activations.py
+++ keras_capsnet/activations.py
@@ -2,5 +2,5 @@ import keras.backend as K
 
 def squash(x, axis=-1):
     s_norm = K.sum(K.square(x), axis, keepdims=True) + K.epsilon()
-    scale = K.sqrt(s_norm) / (1 + s_norm)
+    scale = K.sqrt(s_norm) / (0.5 + s_norm)
     return scale * x
